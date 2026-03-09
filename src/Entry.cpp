24252b0f5945c369f9b51df5d84981cf72af3ca0
farhan <farhankabir2024@gmail.com>
2026-03-09 23:32:33 +0600

    completed template classes for each case 	modified:   src/Entry.cpp 	modified:   src/Entry.hpp

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -34,3 +34,83 @@
 
         return line;
     }
+
+    string Template1 :: get_Request(int a ,  int b , int amount){
+
+        ostringstream oss;
+
+        oss << " User " << a << " gets " << amount << " Taka from User " << b << endl;
+        string line  = oss.str();
+
+        return line;
+    }
+
+    string Template1 :: give_Request(int a , int b , int amount ){
+
+        ostringstream oss;
+
+        oss << " User " << a << " gives " << amount << " Taka to User " << b << endl;
+        string line = oss.str();
+
+        return line;
+    }
+
+    string Template2 :: get_Request( int a , int amount , string arr[] , int num){
+
+        ostringstream oss ;
+        int size = num ;
+
+        while(num--){
+            oss << " User " << a << " gets " << amount/size << " Taka from User " << arr[num-1]  << "\n";
+        }
+
+        string line = oss.str();
+
+        return line;
+    }
+
+    string Template2 :: give_Request(int a , int amount , string arr[] , int num ){
+
+        ostringstream oss ;
+        int size = num ;
+
+        while(num--){
+            oss << " User " << a << " gives " << amount/size << " Taka to User " << arr[num-1]  << "\n";
+        }
+
+        string line = oss.str();
+
+        return line;
+    }
+
+    string Template3 :: get_Request(int a , int amount[] , string arr[], int num){
+
+        ostringstream oss;
+        int size = num;
+
+        
+        while(num--){
+            oss << " User " << a << " gets" << amount[num-1] << " Taka from User " << arr[num-1]  << "\n";
+        }
+
+        string line = oss.str();
+
+        return line;
+    }
+
+    string Template3 :: give_Request(int a , int amount[] , string arr[] ,int num ){
+
+        ostringstream oss;
+        int size = num;
+
+        
+        while(num--){
+            oss << " User " << a << " gives" << amount[num-1] << " Taka to User " << arr[num-1]  << "\n";
+        }
+
+        string line = oss.str();
+
+        return line;
+    }
+
+
--- src/Entry.hpp
+++ src/Entry.hpp
@@ -6,6 +6,8 @@ using namespace std;
 
 class User;
 class Log;
+class Account;
+class Session;
 
 class Entry {
     private : 
@@ -26,6 +28,46 @@ class Entry {
         
 
     string give_Request(int a , int b , int amount ){}
+
+};
+
+class Template1 : public Entry {
+    private:
+    int UserId1;
+    int UserId2;
+
+    public:
+
+    string get_Request(int a ,  int b , int amount){}
+
+    string give_Request(int a , int b , int amount ){}
+
+
+};
+
+class Template2 : public Entry {
+
+    private:
+    int userId;
+
+    public :
+
+    string get_Request(int a , int amount , string arr[] , int num ){}
+
+    string give_Request(int a , int amount , string arr[] , int num ){}
+
+};
+
+class Template3 : public Entry {
+    
+    private :
+    int userId;
+    
+
+    private:
+    string get_Request(int a , int amount[] , string arr[] ,int num){}
+
+    string give_Request(int a , int amount[] , string arr[] ,int num ){}
 
 };
 
